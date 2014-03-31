5f8fd94e7f8d52fdfdd3ac7c1ed0c14b351f2982
Nicolas Franck <nicolas.franck@ugent.be>
2014-03-31 14:59:42 +0200

    first commit

---
+++ lib/Catmandu/Fix/XML.pm
@@ -0,0 +1,18 @@
+package Catmandu::Fix::XML;
+our $VERSION = "0.1";
+
+=head1 NAME
+
+  Catmandu::Fix::Date - Catmandu fixes for processing xml
+  
+=head1 AUTHOR
+
+Nicolas Franck, C<< <nicolas.franck at ugent.be> >>
+
+=head1 SEE ALSO
+
+  L<Catmandu::Fix::xml_transform>
+
+=cut
+
+1;
